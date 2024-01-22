739ff9ca9d1773bec8c9ccf0ad91d8966da51650
vision1v1 <vision1v1@163.com>
2024-01-22 18:28:54 +0800

    优化格式

--- debug_bert/modeling_bert.py
+++ debug_bert/modeling_bert.py
@@ -193,9 +193,7 @@ class BertEmbeddings(nn.Module):
         # position_ids (1, len position emb) is contiguous in memory and exported when serialized
         self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
         self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
-        self.register_buffer(
-            "token_type_ids", torch.zeros(self.position_ids.size(), dtype=torch.long), persistent=False
-        )
+        self.register_buffer("token_type_ids", torch.zeros(self.position_ids.size(), dtype=torch.long), persistent=False)
 
     def forward(
         self,
